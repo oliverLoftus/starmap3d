209fbba6585ea3782806cf4e62028e73a19e1dcb
oliverLoftus <oliloftus@hotmail.com>
2024-11-12 11:21:46 +0000

    workaround for overlapping names

--- stars.js
+++ stars.js
@@ -12,8 +12,8 @@ class Star {
 const stars = [
   new Star("Sol", 0, 0, 0, 0.656, 15),
   new Star("Proxima Centauri", -0.474, -0.363, -1.157, 1.807, 2),
-  new Star("Toliman", -0.495, -0.414, -1.157, 0.9, 13),
-  new Star("Rigil Kentaurus", -0.495, -0.414, -1.157, 0.71, 18),
+  new Star("", -0.495, -0.414, -1.157, 0.9, 13), //Toliman moved label onto Alpha C
+  new Star("Alpha Centauri AB", -0.495, -0.414, -1.157, 0.71, 18),
   new Star("Barnard's Star", -0.017, -1.822, 0.15, 1.57, 3),
   new Star("Wolf 359", -2.3, 0.654, 0.294, 2, 1),
   new Star("Lalande 21185", -1.998, 0.504, 1.495, 1.502, 5),
@@ -25,11 +25,11 @@ const stars = [
   new Star("Lacaille 9352", 2.591, -0.625, -1.926, 1.483, 7),
   new Star("Ross 128", -3.37, 0.181, 0.048, 1.746, 3),
   new Star("EZ Aquarii", 3.077, -1.142, -0.898, 1.98, 3),
-  new Star("61 Cygni B", 1.986, -1.869, 2.188, 1.309, 9),
-  new Star("61 Cygni A", 1.985, -1.87, 2.188, 1.069, 10),
+  new Star("", 1.986, -1.869, 2.188, 1.309, 9), //61 Cygni B moved lable to A
+  new Star("61 Cygni AB", 1.985, -1.87, 2.188, 1.069, 10),
   new Star("Procyon", -1.469, 3.176, 0.32, 0.432, 31),
-  new Star("Struve 2398 A", 0.331, -1.75, 3.04, 1.504, 6),
-  new Star("Struve 2398 B", 0.331, -1.751, 3.039, 1.561, 5),
+  new Star("Struve 2398 AB", 0.331, -1.75, 3.04, 1.504, 6),
+  new Star("", 0.331, -1.751, 3.039, 1.561, 5), //Struve 2398 B moved label to A
   new Star("Groombridge 34", 2.553, 0.205, 2.476, 1.56, 6),
   new Star("GJ 1111", -1.944, 2.538, 1.613, 2.06, 2),
   new Star("Epsilon Indi", 1.74, -0.971, -3.044, 1.056, 10),
